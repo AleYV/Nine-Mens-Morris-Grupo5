440fd28bf58e0ac504e3412224f5e7ead8c993a1
Italo Silva Guanilo <italosilva@uni.pe>
2022-05-09 23:35:35 -0500

    Correcccion logica de tablero

--- src/main/java/controller/NineMensMorrisGame.java
+++ src/main/java/controller/NineMensMorrisGame.java
@@ -18,12 +18,12 @@ public class NineMensMorrisGame {
 
     private void initGame(){
         for (int i=-3; i<=3; i++){
-            for(int j=-3;j<3;j++){
-                if(i!=0 &&  j!=0)
-                    if(Math.abs(i)==Math.abs(j))
-                        table[i+3][j+3] = Cells.EMPTY;
-                if(i==0 && j!=0) table[i+3][j+3] = Cells.EMPTY;
-                if(i!=0 && j==0) table[i+3][j+3] = Cells.EMPTY;
+            for(int j=-3;j<=3;j++){
+                if (i==0 && j==0) table[i+3][j+3] = Cells.DISABLED;
+                else if(Math.abs(i)==Math.abs(j))
+                    table[i+3][j+3] = Cells.EMPTY;
+                else if(i==0 && j!=0) table[i+3][j+3] = Cells.EMPTY;
+                else if(i!=0 && j==0) table[i+3][j+3] = Cells.EMPTY;
                 else table[i+3][j+3] = Cells.DISABLED;
             }
         }
