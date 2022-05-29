f14c8f51ce9c0e378dd3630ae25e717dd39102a0
Renato S. Perez Ruiz <sperezr@uni.pe>
2022-05-29 16:51:55 -0500

    Se crea un test simple para la GUI

---
+++ src/test/java/gui/NineMensMorrisGUITest.java
@@ -0,0 +1,30 @@
+package gui;
+
+import controller.NineMensMorrisGame;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NineMensMorrisGUITest {
+    private NineMensMorrisGame game;
+
+    @Before
+    public void setUp() {
+        game = new NineMensMorrisGame();
+    }
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testEmptyBoard(){
+        new NineMensMorrisGUI(game);
+        try{
+            Thread.sleep(2000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
